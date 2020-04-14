68c38f2a7392cac24f770224207744f184a1c119
lzckira <43661779+lzckira@users.noreply.github.com>
2020-04-14 15:12:58 -0700

    Update lab3.sh

--- lab3.sh
+++ lab3.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-echo '1. Find all files in /bin, /sbin, /usr/sbin that are setuid and owned by root'
+echo '1. Find all files in /bin, /sbin, /usr/bin, and /usr/sbin that are setuid and owned by root.'
 
 
+echo '2. Find all files across the entire system that have setuid or setgid enabled (regardless of owner).'
 
 
+echo '3. Find all files in /var that have changed in the last 20 minutes.'
 
 
+echo '4. Find all files in /var that are regular files of zero length.'
+
+
+echo '5. Find all files in /dev that are not regular files and also not directories. The same command should print a listing that includes permissions and modification times (at a minimum) for these files.'
+
+
+echo '6. Find all directories in /home that are not owned by root. In the same command, change their permissions to ensure they have 711 (-rwx--x--x) permissions.'
+
+
+echo '7. Find all regular files in /home that are not owned by root. In the same command, change their permissions to ensure they have 755 (-rwxr-xr-x) permissions.''
+
+
+echo '8. Find all files (of all types) in /etc that have changed in the last 5 days. 
 
 
 
